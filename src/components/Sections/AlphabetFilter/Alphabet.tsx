8f40d9f521924cdc97d67a179510a40391101aea
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-24 14:54:34 -0300

    fix: add alphabet filter dependencies

---
+++ src/components/Sections/AlphabetFilter/Alphabet.tsx
@@ -0,0 +1,17 @@
+import { AlphabetFilter, AlphabetLetters } from "./style";
+
+export function Alphabet() {
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    return (
+        <AlphabetFilter>
+            <AlphabetLetters>
+                {alphabet.map(alphabet => {
+                    return (
+                        <p>{alphabet}</p>
+                    )
+                })}
+            </AlphabetLetters>
+        </AlphabetFilter>
+    )
+}
