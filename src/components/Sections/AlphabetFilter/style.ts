553299bc46d27251f6b972ec63e40273f08d379e
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-24 14:54:50 -0300

    fix: add alphabet filter style

---
+++ src/components/Sections/AlphabetFilter/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const AlphabetFilter = styled.div`
+width: 100%;
+height: 47px;
+background-color: #F0131E;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.35);
+`
+
+export const AlphabetLetters = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 47px;
+font-size: 20px;
+color: #fff;
+gap: 6px;
+`
