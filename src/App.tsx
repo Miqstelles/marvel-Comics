13f4b319abbd9c0b4ae4aa532fdb40ceecfd9d3e
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-24 13:23:19 -0300

    feat: add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import { useState } from 'react'
+import md5 from 'md5'
+import { Navbar } from './components/Header/Navbar';
+import { Home } from './Pages/Home/Home';
+
+// interface ResponseData {
+//   id: string;
+//   title: string;
+//   description: string;
+//   thumbnail: {
+//     path: string;
+//     extension: string;
+//   }
+// }
+// const publicKey = '82b123107b3f347612d1c147238d3ef7'
+// const privateKey = '437ceec8c6a4281606011b114656643fa8ba8e5f'
+
+// const timeStamp = Number(new Date())
+
+// const hash = md5(timeStamp + privateKey + publicKey)
+
 
 function App() {
 
   return (
-    <div className="App">
+    <div>
+      {/* {comic.map(character => {
+        return (
+          <h1>{character.title}</h1>
+        )
+      })} */}
+      <Navbar />
+      <Home />
     </div>
   )
 }
