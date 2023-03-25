56f036862120b8f3c0e8a21ea03255784a6e7932
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-25 16:22:39 -0300

    fix: add loading component dependencies

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import Logo from '../../assets/logo.png'
 import { Loading } from "./style";
+import { useNavigate } from 'react-router-dom'
+import { Home } from "../../Pages/Home/Home";
+
 
 export function LoadingScreen() {
     const [loadingPercentage, setLoadingPercentage] = useState(0);
@@ -17,11 +20,12 @@ export function LoadingScreen() {
 
     const isLoadingComplete = loadingPercentage >= 100;
 
+
     if (!isLoadingComplete) {
         return (
             <Loading
                 style={{
-                    backgroundColor: isLoadingComplete ? "white" : "#F0131E",
+                    backgroundColor: "#F0131E",
                 }}
             >
                 <img src={Logo} alt="" width={220} height={90} style={{ opacity: `${loadingPercentage + 20}%`, filter: 'drop-shadow(0px 4px 4px #00000040)' }} />
@@ -29,8 +33,10 @@ export function LoadingScreen() {
         )
     }
     return (
-        <div></div>
+        <Home />
     )
+
+
 };
 
 export default LoadingScreen;
