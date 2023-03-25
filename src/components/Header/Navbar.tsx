1544cc364bd343410282b1d15c2c7cffabcdbe05
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-25 16:21:55 -0300

    feat: add routes to home component

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,13 +1,14 @@
 import { ShoppingCartSimple, MagnifyingGlass } from "@phosphor-icons/react"
 import { Img, Navb, Ul } from "./style"
 import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom"
 
 export function Navbar() {
     return (
         <Navb>
             <Ul>
                 <li style={{ marginTop: '5px' }}><ShoppingCartSimple size={32} color={'#fff'} /></li>
-                <li><Img src={logo} alt="Marvel Logo" /></li>
+                <Link to={`/Home`}> <li><Img src={logo} alt="Marvel Logo" /></li></Link>
                 <li style={{ marginTop: '5px' }}><MagnifyingGlass size={32} color={'#fff'} /></li>
             </Ul>
         </Navb>
