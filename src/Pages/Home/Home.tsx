5e08fae967f9035c8cb881d0461cd76f5744d40f
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-25 16:24:46 -0300

    fix: add home component dependencies

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { Carousel } from "../../components/Caroulsel/Carousel";
+import { Navbar } from "../../components/Header/Navbar";
 import { Comics } from "../../components/Sections/Comics/Comics";
 
 export function Home() {
     return (
         <div>
+            <Navbar />
             <Carousel />
             <Comics />
         </div>
