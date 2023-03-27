af5e9101b99b355e698c0b296e2298852a3f2256
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-26 22:08:44 -0300

    fix: add comic home dependencies

--- src/components/Sections/Comics/Comics.tsx
+++ src/components/Sections/Comics/Comics.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import md5 from "md5";
 import { useEffect, useState } from "react";
-import { Alphabet } from "../AlphabetFilter/Alphabet";
-import { ComicCard, ComicsDiv, ComicTitle } from "./styles";
+import { ActivateLetter, AlphabetFilter, AlphabetLetters, ButtonClear, ButtonLetter, ComicCard, ComicImg, ComicsDiv, ComicTitle, PageButton, PageButtons } from "./styles";
 import { Link } from 'react-router-dom';
 
 export interface ResponseData {
@@ -15,8 +14,8 @@ export interface ResponseData {
     }
 }
 
-const publicKey = '82b123107b3f347612d1c147238d3ef7'
-const privateKey = '437ceec8c6a4281606011b114656643fa8ba8e5f'
+const publicKey = '7c05f6b7e459ef60c26039c3085eeea9'
+const privateKey = '6e09d9074c633fd86c3f24bff4e736b82b42b768'
 
 const timeStamp = Number(new Date())
 
@@ -24,6 +23,13 @@ const hash = md5(timeStamp + privateKey + publicKey)
 
 export function Comics() {
     const [comics, setComic] = useState<ResponseData[]>([])
+
+    const [itemsData, setItemsData] = useState<ResponseData[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [itemsPerPage, setItemsPerPage] = useState<number>(8);
+    const [selectedPage, setSelectedPage] = useState<number>(currentPage);
+
+    const [activeLetter, setActiveLetter] = useState<string>("");
 
     useEffect(() => {
         axios
@@ -34,21 +40,73 @@ export function Comics() {
             .catch(err => console.log(err))
     }, []);
 
+    useEffect(() => {
+        const filteredItems = activeLetter ? comics.filter(comic => comic.title.startsWith(activeLetter)) : comics;
+        setItemsData(filteredItems);
+        setCurrentPage(1);
+    }, [activeLetter, comics]);
+
+    const handleLetterClick = (letter: string) => {
+        setActiveLetter(letter);
+    };
+
+    const handlePageClick = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const lastIndex = currentPage * itemsPerPage;
+    const firstIndex = lastIndex - itemsPerPage;
+    const currentItems = itemsData.slice(firstIndex, lastIndex);
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(itemsData.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
     return (
         <div>
-            <Alphabet />
+            <AlphabetFilter>
+                <AlphabetLetters>
+                    {Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i)).map(
+                        letter => (
+                            <ButtonLetter title={letter} key={letter} onClick={() => handleLetterClick(letter)}>
+                                {letter}
+                            </ButtonLetter>
+                        )
+                    )}
+
+                </AlphabetLetters>
+            </AlphabetFilter>
+            <ActivateLetter>{activeLetter} {activeLetter ? <ButtonClear onClick={() => setActiveLetter("")}>LIMPAR</ButtonClear> : null}</ActivateLetter>
             <div style={{ display: "flex", justifyContent: 'center' }}>
                 <ComicsDiv>
-                    {comics.map(comic => {
+                    {currentItems.map(comic => {
                         return (
-                            <ComicCard>
-                                <Link title={comic.title} to={`/ComicCart/${comic.id}`}  ><img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} width={140} height={200} /></Link>
-                                <ComicTitle>{comic.title}</ComicTitle>
-                            </ComicCard>
+                            <div key={comic.id}>
+                                <ComicCard>
+                                    <Link title={comic.title} to={`/ComicDetails/${comic.id}`}  ><ComicImg src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} /></Link>
+                                    <ComicTitle>{comic.title}</ComicTitle>
+                                </ComicCard>
+                            </div>
                         )
                     })}
                 </ComicsDiv>
             </div>
+            <PageButtons>
+                {pageNumbers.map(number => (
+                    <PageButton
+                        key={number}
+                        onClick={() => {
+                            handlePageClick(number);
+                            setSelectedPage(number);
+                        }}
+                        disabled={currentPage === number}
+                        style={selectedPage === number ? { backgroundColor: '#F0131E', color: '#fff', borderRadius: '5px' } : {}}
+                    >
+                        {number}
+                    </PageButton>
+                ))}
+            </PageButtons>
         </div >
     )
 
---
+++ src/components/Sections/Comics/styles.ts
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+
+export const ComicsDiv = styled.div`
+display: grid;
+justify-content: center;
+align-items: center;
+grid-template-columns: repeat(2,1fr);
+grid-gap: 48px;
+@media only screen and (min-width: 760px){
+    grid-template-columns: repeat(4, 0.1fr);
+    grid-row-gap: 108px;
+}
+`
+export const ComicCard = styled.div`
+width: 140px;
+height: 250px;
+@media only screen and (min-width: 760px){
+    width: 260px;
+    height: 480px;
+}
+`
+
+export const ComicImg = styled.img`
+width: 140px;
+height: 200px;
+@media only screen and (min-width: 760px){
+    width: 280px;
+    height: 360px;
+}
+`
+
+export const ComicTitle = styled.h1`
+font-size: 20px;
+@media only screen and (min-width: 760px){
+   font-size: 32px;
+}
+`
+
+export const AlphabetFilter = styled.div`
+width: 100%;
+height: 47px;
+background-color: #F0131E;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.35);
+@media only screen and (min-width: 760px){
+    height: 60px;
+}
+`
+
+export const AlphabetLetters = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 47px;
+font-size: 20px;
+color: #fff;
+gap: 6px;
+@media only screen and (min-width: 760px){
+    height: 60px;
+    font-size: 32px;
+    gap: 26px;
+}
+`
+
+export const Letter = styled.a`
+text-decoration: none;
+`
+
+export const ButtonLetter = styled.button`
+background: none;
+color: inherit;
+border: none;
+padding: 0;
+font: inherit;
+cursor: pointer;
+`
+
+export const ButtonClear = styled.button`
+background: none;
+color: inherit;
+border: 2px solid #F0131E;
+border-radius: 5px 15px;
+padding: 5px;
+font: inherit;
+font-size: 28px;
+cursor: pointer;
+&:hover{
+    background-color: #F0131E;
+    color: #fff;
+    transition: 0.6s;
+}
+@media only screen and (min-width: 760px){
+   font-size: 56px;
+   width: 175px;
+   height: 82px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+`
+
+export const ActivateLetter = styled.div`
+color: #F0131E;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 20px;
+font-size: 48px;
+text-align: center;
+margin-top: 36px;
+margin-bottom: 36px;
+@media only screen and (min-width: 760px){
+   font-size: 128px;
+}
+`
+
+export const PageButtons = styled.div`
+display: flex;
+justify-content: center;
+margin-bottom: 38px;
+margin-top: 40px;
+gap: 24px;
+@media only screen and (min-width: 760px){
+   margin-top: 100px;
+}
+`
+
+export const PageButton = styled.button`
+border: none;
+font-size: 48px;
+font-family: 'Bebas Neue';
+background-color: transparent;
+color: #F0131E;
+justify-content: center;
+margin-top: 38px;
+@media only screen and (min-width: 760px){
+    font-size: 64px;
+}
+`
