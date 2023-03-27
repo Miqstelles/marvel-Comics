32d76c53eb32ca1101292806433aecef1f811832
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-26 22:06:33 -0300

    fix: add new carousel dependencies

--- src/components/Caroulsel/Carousel.tsx
+++ src/components/Caroulsel/Carousel.tsx
@@ -1,6 +1,5 @@
-import { CarouselDiv, CarouselContent, CarouselInfos, Info, Title, BtnComprar, SliderPaginataion, SliderPage } from './style'
+import { CarouselDiv, CarouselContent, CarouselInfos, Info, Title, BtnComprar, SliderPaginataion, SliderPage, Arrow } from './style'
 import { CaretRight } from '@phosphor-icons/react'
-
 
 export function Carousel() {
     return (
@@ -9,14 +8,15 @@ export function Carousel() {
                 <CarouselInfos>
                     <Title>'scarlet witch annual' #1</Title>
 
-                    <Info>Agatha Harkness Conjures Up a
+                    <Info>
+                        Agatha Harkness Conjures Up a
                         'Contest of Chaos'
                     </Info>
 
                     <BtnComprar>Comprar</BtnComprar>
                 </CarouselInfos>
 
-                <CaretRight style={{ marginTop: '126px', color: "#fff" }} size={48} weight="regular" />
+                <Arrow><CaretRight weight='regular' size={78} /></Arrow>
             </CarouselContent>
 
             <SliderPaginataion>
