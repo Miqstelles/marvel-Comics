211952dcc0d3ac451f1fc58f6d06ccd777615038
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-03-26 22:06:55 -0300

    fix: add carousel responsive design

--- src/components/Caroulsel/style.ts
+++ src/components/Caroulsel/style.ts
@@ -8,11 +8,19 @@ background-image: url(${scarlet});
 background-size: cover;
 display: grid;
 justify-items: center;
+@media only screen and (min-width: 760px){
+    height: 442px;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
 `
 
 export const CarouselContent = styled.div`
 display: flex;
 justify-content: center;
+@media only screen and (min-width: 760px){
+   gap: 580px;
+}
 `
 
 export const CarouselInfos = styled.div`
@@ -21,15 +29,26 @@ height: 175px;
 margin-top: 32px;
 color: #fff;
 letter-spacing: 1px;
+@media only screen and (min-width: 760px){
+    width: 463px;
+    height: 175px;
+}
 `
 
 export const Title = styled.h1`
 font-size: 28px;
+@media only screen and (min-width: 760px){
+    margin-top: 14px;
+    font-size: 48px;
+}
 `
 
 export const Info = styled.h2`
 font-size: 24px;
 margin-top: 12.4px;
+@media only screen and (min-width: 760px){
+    font-size: 44px;
+}
 `
 
 export const BtnComprar = styled.button`
@@ -46,6 +65,22 @@ font-family: 'Bebas Neue';
     color: #000;
     transition: 0.6s;
 }
+@media only screen and (min-width: 760px){
+    width: 286px;
+    height: 66px;
+    font-size: 40px;
+    margin-top: 40px;
+}
+`
+
+export const Arrow = styled.div`
+margin-top: 126px;
+color: #fff;
+width: 48px;
+height: 48px;
+@media only screen and (min-width: 760px){
+  margin-top: 193px;
+}
 `
 
 export const SliderPaginataion = styled.ul`
@@ -57,6 +92,12 @@ align-items: center;
 justify-content: center;
 padding: 0;
 margin-top: 55px;
+@media only screen and (min-width: 760px){
+    width: 250px;
+    height: 7px;
+    gap: 20px;
+    margin-top: 160px;
+}
 `
 
 export const SliderPage = styled.li`
@@ -64,4 +105,9 @@ width: 18px;
 height: 5px;
 background-color: #fff;
 list-style: none;
+@media only screen and (min-width: 760px){
+    width: 32px;
+    height: 8px;
+}
 `
+
